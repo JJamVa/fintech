62bde71bee430db2049e5e9f6a38b74a21c5bce1
JJamVa <hongi97@naver.com>
2023-03-13 17:29:10 +0800

    props를 이용하여 호출

--- Practice/my-app/src/App.js
+++ Practice/my-app/src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 
-const Welcome = () => {
-  return <p>안녕하세요</p>;
+const Welcome = (props) => {
+  console.log(props);
+  return <p>안녕하세요. {props.userName}님 나이가 {props.userAge}이며, {props.userSchool}을 다니시군요.</p>;
 }
 
 function App() {
   return (
     <div className="App">
-      <Welcome></Welcome>
+      <Welcome userName="JJamVa" userAge={27} userSchool="숭실대학교"></Welcome>
+      {/* property */}
     </div>
   );
 }
