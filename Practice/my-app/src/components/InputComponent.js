957b647a7150878465f35e643d6a2d1927bb6409
JJamVa <hongi97@naver.com>
2023-03-15 02:35:09 +0800

    state를 이용하여 화면 랜더링

--- Practice/my-app/src/components/InputComponent.js
+++ Practice/my-app/src/components/InputComponent.js
@@ -2,8 +2,13 @@ import { useState } from "react";
 
 // react state사용하기
 const InputComponents = (props) => {
+  //let value = "test"
   const handleChangeInput = (event) => {//입력이 변경될 때 마다 동작
     console.log(event.target.value);
+    setName(event.target.value)//리랜더링작업이 가능하다. 즉, 즉각적인 화면 표출이 가능
+  }
+  const handleClick = () => {
+    window.alert(name + "를 누르셨습니다.");
   }
   const [name, setName] = useState("hello");
   //name은 변수명, setName은 name을 바꾸기 위한 메소드명이 들어간다
@@ -11,7 +16,7 @@ const InputComponents = (props) => {
     <>
       <p>{name}</p>
       <input onChange={handleChangeInput}></input>
-      <button>입력</button>
+      <button onClick={handleClick}>입력</button>
     </>
   );
 }
