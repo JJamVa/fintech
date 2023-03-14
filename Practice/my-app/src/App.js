19d09d696847e5e569cffe182866605ba4cace94
JJamVa <hongi97@naver.com>
2023-03-14 22:56:18 +0800

    import를 통하여 호출

--- Practice/my-app/src/App.js
+++ Practice/my-app/src/App.js
@@ -1,17 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/Welcome'
 
-
-const Welcome = (props) => {
-  console.log(props);
-  return <p>안녕하세요. {props.userName}님 나이가 {props.userAge}이며, {props.userSchool}을 다니시군요.</p>;
-}
 
 function App() {
   return (
     <div className="App">
       <Welcome userName="JJamVa" userAge={27} userSchool="숭실대학교"></Welcome>
+      <Welcome userName="JJam" userAge={26} userSchool="숭실대학교"></Welcome>
+      <Welcome userName="Va" userAge={25} userSchool="숭실대학교"></Welcome>
       {/* property */}
+      {/* class를 지양 function을 지향 */}
     </div>
   );
 }
