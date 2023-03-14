d0b77d8731c6b6350b16e3156641f3a1714620a2
JJamVa <hongi97@naver.com>
2023-03-15 02:25:54 +0800

    input에 따른 onChange 바인딩

--- Practice/my-app/src/components/InputComponent.js
+++ Practice/my-app/src/components/InputComponent.js
@@ -1,11 +1,19 @@
 import { useState } from "react";
 
-useState
 // react state사용하기
 const InputComponents = (props) => {
-    const [name, setName] = useState("test");//초기값 설정
-    //name은 변수명, setName은 name을 바꾸기 위한 메소드명이 들어간다
-    return <p>{name}</p>;
+  const handleChangeInput = (event) => {//입력이 변경될 때 마다 동작
+    console.log(event.target.value);
   }
+  const [name, setName] = useState("hello");
+  //name은 변수명, setName은 name을 바꾸기 위한 메소드명이 들어간다
+  return (
+    <>
+      <p>{name}</p>
+      <input onChange={handleChangeInput}></input>
+      <button>입력</button>
+    </>
+  );
+}
 
-  export default InputComponents;
+export default InputComponents;
