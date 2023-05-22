12498af142e3f0509dc8e660536a76e559ec750e
JJamVa <hongi97@naver.com>
2023-05-22 15:23:07 +0900

    reset

--- Practice/my-app/src/components/Welcome.js
+++
@@ -1,6 +0,0 @@
-const Welcome = (props) => {
-    console.log(props);
-    return <p>안녕하세요. {props.userName}님 나이가 {props.userAge}이며, {props.userSchool}을 다니시군요.</p>;
-  }
-
-  export default Welcome;
