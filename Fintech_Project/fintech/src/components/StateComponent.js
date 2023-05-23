5f8a6e0903fffa98c05580c2e386ab02985f93ef
JJamVa <hongi97@naver.com>
2023-05-23 17:00:49 +0900

    useState Practice

---
+++ Fintech_Project/fintech/src/components/StateComponent.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const StateComponent = () => {
+    let userName = "홍길동"
+    let [count, setCount] = useState(0)
+
+    setInterval(() => {
+        console.log("카운트를 변경합니다. ",count)
+        setCount(count++)
+    },3000)//userCount의 내부적으로 데이터는 변경이 되었으나, 실질적으로는 화면에서는 변경이 되지않음 그러나 useState를 이용하면 변경이 가능함.
+
+    return (
+        <div>{userName}</div>
+    )
+}
+
+export default StateComponent
